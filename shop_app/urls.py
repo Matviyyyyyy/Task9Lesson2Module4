8adf96bbc7b70fe54a2d3048e6762a3b95ea36dd
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-07-08 13:51:00 +0300

    New views about products and reviews

---
+++ shop_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from shop_app import views
+
+urlpatterns = [
+    path("", views.product_list, name = "products_list"),
+    path("<int:product_id>/", views.get_product_by_id, name = "product-details"),
+]
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from shop_app.models import Product, Review
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        "products": products,
+    }
+    return render(
+        request,
+        template_name="shop/products_page.html",
+        context=context,
+    )
+
+def get_product_by_id(request, product_id):
+    product = Product.objects.get(id = product_id)
+    reviews = Review.objects.filter(product=product).all()
+    context = {
+        "product": product,
+        "reviews": reviews,
+    }
+    return  render(
+        request,
+        template_name="shop/product_details.html",
+        context=context,
+    )
