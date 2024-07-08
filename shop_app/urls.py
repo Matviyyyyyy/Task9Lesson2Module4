7ce69988c41a0a9ae0e2c25f5b68e3e0302a2397
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-07-08 14:33:47 +0300

    Add view, that adds review to product, few HTMLs and actually requirements

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -4,4 +4,5 @@ from shop_app import views
 urlpatterns = [
     path("", views.product_list, name = "products_list"),
     path("<int:product_id>/", views.get_product_by_id, name = "product-details"),
+    path("add-review/<int:product_id>", views.add_review, name = "add-review"),
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from shop_app.models import Product, Review
+from django.http import HttpResponse
 
 def product_list(request):
     products = Product.objects.all()
@@ -24,3 +25,42 @@ def get_product_by_id(request, product_id):
         template_name="shop/product_details.html",
         context=context,
     )
+
+def add_review(request, product_id):
+    if request.method == "POST":
+        review_author = request.POST.get("review_author")
+        review_text = request.POST.get("review_text")
+        review_rating = request.POST.get("review_rating")
+
+        try:
+            product = Product.objects.get(id = product_id)
+        except ValueError:
+            return HttpResponse(
+                "Wrong value for product",
+                status=400
+            )
+        except Product.DoesNotExist:
+            return HttpResponse(
+                "This product doesn't exist",
+                status=404
+            )
+        new_review = Review.objects.create(
+            product = product,
+            author = review_author,
+            text = review_text,
+            rating = review_rating
+        )
+
+        return redirect("product-details", product_id = product.id)
+
+
+
+
+
+
+
+
+
+
+
+
